7c031312b0e6cb95a742c5068a2fdbeb76b87eff
matus.hozza <matus.hozza@globallogic.com>
2017-12-08 10:09:13 +0100

    Je*at na fix. Zadme specialne znaky nebudu. Do pi*i s tou slovencinou.

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -9,42 +9,42 @@ CREATE TABLE IF NOT EXISTS question(
 INSERT INTO question(id, content, answer)
 VALUES
   (
-    1, 'Doplň porekadlo. Sýty hladnému…',
+    1, 'Dopln porekadlo. Syty hladnemu…',
     'neveri'
   );
 /* INSERT QUERY */
 INSERT INTO question(id, content, answer)
 VALUES
   (
-    2, 'Koľko meria najvyšší vrch najväčšieho európského štátu (metre)',
+    2, 'Kolko meria najvyssi vrch najvacsieho európskeho statu (metre)',
     5642
   );
 /* INSERT QUERY */
 INSERT INTO question(id, content, answer)
 VALUES
   (
-    3, 'Prvé meno, druhého manžela, známej americkej herečky, ktorá zomrela 5.9.1962',
+    3, 'Prve meno, druheho manzela, znamej americkej herecky, ktora zomrela 5.9.1962',
     'Joe'
   );
 /* INSERT QUERY */
 INSERT INTO question(id, content, answer)
 VALUES
   (
-    4, 'Názov najznámejšej skladby z druhého albumu speváčky, ktorá sa narodila 14. septembra 1983',
+    4, 'Nazov najznamejsej skladby z druheho albumu spevacky, ktora sa narodila 14. septembra 1983',
     'Rehab'
   );
 /* INSERT QUERY */
 INSERT INTO question(id, content, answer)
 VALUES
   (
-    5, 'Prvé slovo z názvu filmu, v ktorom Eva Mázikova tlačila kapustu nohami.',
+    5, 'Prve slovo z nazvu filmu, v ktorom Eva Mazikova tlacila kapustu nohami.',
     'Pacho'
   );
 /* INSERT QUERY */
 INSERT INTO question(id, content, answer)
 VALUES
   (
-    6, 'Koľko účastníkov sa prihlásilo tento rok na medzinárodný maratón mieru tento rok? ',
+    6, 'Kolko ucastnikov sa prihlasilo tento rok na medzinarodny maratón mieru tento rok? ',
     13010
   );
 /* INSERT QUERY */
@@ -52,7 +52,7 @@ INSERT INTO question(id, content, answer)
 VALUES
   (
     7, '
-Priezvisko moderátora známej oldschulovej relácie “Repete” ',
+Priezvisko moderatora znamej oldschulovej relacie “Repete” ',
     'Krajicek'
   );
 /* INSERT QUERY */
@@ -66,42 +66,42 @@ VALUES
 INSERT INTO question(id, content, answer)
 VALUES
   (
-    9, 'Aký uhol (v stupňoch) opíše sekundová ručička za pol minúty?',
+    9, 'Aky uhol (v stupnoch) opise sekundova rucicka za pol minuty?',
     180
   );
 /* INSERT QUERY */
 INSERT INTO question(id, content, answer)
 VALUES
   (
-    10, 'Ktorému hrdinovi bolo povedané: S veľkou mocou prichádza veľká zodpovednosť?',
+    10, 'Ktoremu hrdinovi bolo povedane: S velkou mocou prichadza velka zodpovednost?',
     'Spiderman'
   );
 /* INSERT QUERY */
 INSERT INTO question(id, content, answer)
 VALUES
   (
-    11, 'Koľko kolies má spravidla vysokozdvižný vozík?',
+    11, 'Kolko kolies ma spravidla vysokozdvizny vozik?',
     4
   );
 /* INSERT QUERY */
 INSERT INTO question(id, content, answer)
 VALUES
   (
-    12, 'Koľko dievčat tvorilo skupinu, ktorá vzniklad v roku 1994 v Anglicku?',
+    12, 'Kolko dievcat tvorilo skupinu, ktora vzniklad v roku 1994 v Anglicku?',
     5
   );
 /* INSERT QUERY */
 INSERT INTO question(id, content, answer)
 VALUES
   (
-    13, 'Názov funkcie človeka stojaceho na čele fakulty vysokej školy',
+    13, 'Nazov funkcie cloveka stojaceho na cele fakulty vysokej skoly',
     'Dekan'
   );
 /* INSERT QUERY */
 INSERT INTO question(id, content, answer)
 VALUES
   (
-    14, 'Kto naspieval titulnú pieseň k seriálu, ktorý mal 236 častí a 10 sérii?',
+    14, 'Kto naspieval titulnu piesen k serialu, ktory mal 236 casti a 10 serii?',
     'The Rembrandts'
   );
 /* INSERT QUERY */
@@ -114,105 +114,105 @@ VALUES
 INSERT INTO question(id, content, answer)
 VALUES
   (
-    16, 'Obec, nachádzajúca sa v okrese Příbram, ktorá ma 568 obyvateľov',
+    16, 'Obec, nachadzajuca sa v okrese Pribram, ktora ma 568 obyvatelov',
     'Picin'
   );
 /* INSERT QUERY */
 INSERT INTO question(id, content, answer)
 VALUES
   (
-    17, 'Z akého filmu je toto GIF? (oficiálny anglický názov) http://gph.is/2c6UdX2',
+    17, 'Z akeho filmu je toto GIF? (oficialny anglicky nazov) http://gph.is/2c6UdX2',
     'The Shining'
   );
 /* INSERT QUERY */
 INSERT INTO question(id, content, answer)
 VALUES
   (
-    18, 'Autor citátu Truly wonderful, the mind of a child is.',
+    18, 'Autor citatu Truly wonderful, the mind of a child is.',
     'Yoda'
   );
 /* INSERT QUERY */
 INSERT INTO question(id, content, answer)
 VALUES
   (
-    19, 'Predmet, do ktorého Voldemort ukryl časť svojej duše',
+    19, 'Predmet, do ktoreho Voldemort ukryl cast svojej duse',
     'horcrux'
   );
 /* INSERT QUERY */
 INSERT INTO question(id, content, answer)
 VALUES
   (
-    20, 'Krstné meno autora obrazu: https://wallpapercave.com/wp/FUA1WNB.jpg',
+    20, 'Krstne meno autora obrazu: https://wallpapercave.com/wp/FUA1WNB.jpg',
     'Leonardo'
   );
 /* INSERT QUERY */
 INSERT INTO question(id, content, answer)
 VALUES
   (
-    21, 'Priezvisko vynálezcu hry Dungeons and Dragons',
+    21, 'Priezvisko vynalezcu hry Dungeons and Dragons',
     'Gygax'
   );
 /* INSERT QUERY */
 INSERT INTO question(id, content, answer)
 VALUES
   (
-    22, 'Značka akutálne najrýchlejšieho auta na svete. ',
+    22, 'Znacka akutalne najrychlejsieho auta na svete. ',
     'Bugatti'
   );
 /* INSERT QUERY */
 INSERT INTO question(id, content, answer)
 VALUES
   (
-    23, 'Koľko krajín tvorí ostrov Veľká Británia?',
+    23, 'Kolko krajin tvori ostrov Velka Britania?',
     4
   );
 /* INSERT QUERY */
 INSERT INTO question(id, content, answer)
 VALUES
   (
-    24, 'Krstné meno Darth Vadera.',
+    24, 'Krstne meno Darth Vadera.',
     'Anakin'
   );
 /* INSERT QUERY */
 INSERT INTO question(id, content, answer)
 VALUES
   (
-    25, 'Kde sa nachádza najväčšia púšť sveta?',
+    25, 'Kde sa nachadza najvacsia pust sveta?',
     'Antarktida'
   );
 /* INSERT QUERY */
 INSERT INTO question(id, content, answer)
 VALUES
   (
-    26, 'Názov ovocia na obrázku: https://www.kamnajedlo.eu/storage/app/media/old/1437463782.jpg',
+    26, 'Nazov ovocia na obrazku: https://www.kamnajedlo.eu/storage/app/media/old/1437463782.jpg',
     'Lici'
   );
 /* INSERT QUERY */
 INSERT INTO question(id, content, answer)
 VALUES
   (
-    27, 'Aká spoločnosť vyvinula programovací jazyk Swift?',
+    27, 'Aka spolocnost vyvinula programovaci jazyk Swift?',
     'Apple'
   );
 /* INSERT QUERY */
 INSERT INTO question(id, content, answer)
 VALUES
   (
-    28, 'Panorámu akého mesta vidíte? https://techtour-prod-public.s3-eu-west-1.amazonaws.com/1203101?AWSAccessKeyId=AKIAIQY2UYNCMITUGSSA&Expires=1513198159&Signature=xzJEXjMKxZOXUWGPpqjdcTk5PKU%3D',
+    28, 'Panoramu akeho mesta vidite? https://techtour-prod-public.s3-eu-west-1.amazonaws.com/1203101?AWSAccessKeyId=AKIAIQY2UYNCMITUGSSA&Expires=1513198159&Signature=xzJEXjMKxZOXUWGPpqjdcTk5PKU%3D',
     'Istanbul'
   );
 /* INSERT QUERY */
 INSERT INTO question(id, content, answer)
 VALUES
   (
-    29, 'Na logo akého auta sa práve pozeráte? https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRkqO0ny4hVYoNPthPJP4-bPfcKcQxAxNDe6n9-mAPzTBWn1dXgAQ',
+    29, 'Na logo akeho auta sa prave pozerate? https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRkqO0ny4hVYoNPthPJP4-bPfcKcQxAxNDe6n9-mAPzTBWn1dXgAQ',
     'Cadillac'
   );
 /* INSERT QUERY */
 INSERT INTO question(id, content, answer)
 VALUES
   (
-    30, 'Podľa akého druhu zvieraťa sú pomenované Kanárske ostrovy?',
+    30, 'Podla akeho druhu zvierata su pomenovane Kanarske ostrovy?',
     'pes'
   );
 /* INSERT QUERY */
@@ -226,56 +226,56 @@ VALUES
 INSERT INTO question(id, content, answer)
 VALUES
   (
-    32, 'Priezvisko herca v známom GIF: https://i.imgur.com/Oe7KX0d.mp4',
+    32, 'Priezvisko herca v znamom GIF: https://i.imgur.com/Oe7KX0d.mp4',
     'Hill'
   );
 /* INSERT QUERY */
 INSERT INTO question(id, content, answer)
 VALUES
   (
-    33, 'Ktorému štátu patrí táto vlajka? http://www.gamesforthebrain.com/game/flag/image/kt-lgflag.gif',
+    33, 'Ktoremu statu patri tato vlajka? http://www.gamesforthebrain.com/game/flag/image/kt-lgflag.gif',
     'Australia'
   );
 /* INSERT QUERY */
 INSERT INTO question(id, content, answer)
 VALUES
   (
-    34, 'Kto stojí vo WOW proti Aliancii?',
+    34, 'Kto stoji vo WOW proti Aliancii?',
     'horda'
   );
 /* INSERT QUERY */
 INSERT INTO question(id, content, answer)
 VALUES
   (
-    35, 'Akú knižnicu vyvinul Facebook ako alternatívu ku Angularu?',
+    35, 'Aku kniznicu vyvinul Facebook ako alternativu ku Angularu?',
     'React'
   );
 /* INSERT QUERY */
 INSERT INTO question(id, content, answer)
 VALUES
   (
-    36, 'Značka luxusných átu, ktorej 100% vlastníkom je Fiat Group.',
+    36, 'Znacka luxusnych atu, ktorej 100% vlastnikom je Fiat Group.',
     'Maserati'
   );
 /* INSERT QUERY */
 INSERT INTO question(id, content, answer)
 VALUES
   (
-    37, 'Priezvisko človeka, ktorý rozlúštil Enigmu.',
+    37, 'Priezvisko cloveka, ktory rozlustil Enigmu.',
     'Turing'
   );
 /* INSERT QUERY */
 INSERT INTO question(id, content, answer)
 VALUES
   (
-    38, 'Na tvar akého štátu sa práve pozeráte? https://lh4.googleusercontent.com/pOxX_5owtdbABY6E-xda58tleTQZOfaD693VDTYh5qO6War9nI_hwxz2r-iTVrjBO3CHsT-dXWhl51mLjrKONrHP_n2sXdxR0EIH2T7yFUIfbp8vfK2Yg5QM',
+    38, 'Na tvar akeho statu sa prave pozerate? https://lh4.googleusercontent.com/pOxX_5owtdbABY6E-xda58tleTQZOfaD693VDTYh5qO6War9nI_hwxz2r-iTVrjBO3CHsT-dXWhl51mLjrKONrHP_n2sXdxR0EIH2T7yFUIfbp8vfK2Yg5QM',
     'Kanada'
   );
 /* INSERT QUERY */
 INSERT INTO question(id, content, answer)
 VALUES
   (
-    39, 'Priezvisko autora tabuľky chemických prvkov.',
+    39, 'Priezvisko autora tabulky chemickych prvkov.',
     'Mendelejev'
   );
 /* INSERT QUERY */
@@ -289,14 +289,14 @@ VALUES
 INSERT INTO question(id, content, answer)
 VALUES
   (
-    41, 'Nedávno zavretú kartu v prehliadači opäť otvoríš, keď stlačíť Ctrl+Shift+?',
+    41, 'Nedavno zavretu kartu v prehliadaci opat otvoris, keď stlacit Ctrl+Shift+?',
     'T'
   );
 /* INSERT QUERY */
 INSERT INTO question(id, content, answer)
 VALUES
   (
-    42, 'Aký bude výstup, ak funkciu mystery použijeme na reťazci Frontend masters?
+    42, 'Aky bude vystup, ak funkciu mystery pouzijeme na retazci Frontend masters?
 String.prototype.mystery=function(){var t,r=0;if(0===this.length)return r;for(t=0;t<this.length;t++)r=(r<<5)-r+this.charCodeAt(t),r|=0;return r};',
     1750097507
   );
@@ -338,14 +338,14 @@ VALUES
 INSERT INTO question(id, content, answer)
 VALUES
   (
-    46, 'Napíš skrátený názov parametra programu rsync, ktorý len zobrazí čo by sa synchronizovalo. Bez pomlčky',
+    46, 'Napis skrateny nazov parametra programu rsync, ktory len zobrazi co by sa synchronizovalo. Bez pomlcky',
     'n'
   );
 /* INSERT QUERY */
 INSERT INTO question(id, content, answer)
 VALUES
   (
-    47, 'Tento hash vyzerá povedome.
+    47, 'Tento hash vyzera povedome.
 c2750a7d522eb4df4e842980ed3f3e78',
     'nbusr123'
   );
@@ -389,28 +389,28 @@ VALUES
 INSERT INTO question(id, content, answer)
 VALUES
   (
-    51, 'Do odpovede napíš quicksort v akomkolvek programovacom jazyku.',
+    51, 'Do odpovede napis quicksort v akomkolvek programovacom jazyku.',
     'quicksort v akomkolvek programovacom jazyku'
   );
 /* INSERT QUERY */
 INSERT INTO question(id, content, answer)
 VALUES
   (
-    52, 'Obľúbený programátorský had (angl.).',
+    52, 'Oblubeny programatorsky had (angl.).',
     'python'
   );
 /* INSERT QUERY */
 INSERT INTO question(id, content, answer)
 VALUES
   (
-    53, 'Koľko jabĺk v ústach je možné si počas jednej minúty rozrezať motorovou pílou? (rekord)',
+    53, 'Kolko jabĺk v ustach je mozne si pocas jednej minuty rozrezat motorovou pilou? (rekord)',
     21
   );
 /* INSERT QUERY */
 INSERT INTO question(id, content, answer)
 VALUES
   (
-    54, 'Cézar o päť
+    54, 'Cezar o pat
 hjqd yjsyt yjcy oj utywjgsj ify it tiutajij',
     'cely tento text je potrebne dat do odpovede'
   );
